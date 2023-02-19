3fbf46d27f6e2d7f57fd9f109f520c66cadb085d
PokyCookie <0cookieboy0@gmail.com>
2023-02-20 00:32:12 +0900

    mmdf

--- src/components/mmdf/mmdfReader.tsx
+++ src/components/mmdf/mmdfReader.tsx
@@ -1,12 +1,61 @@
 import { db } from "../../db";
 import { IMMDF, IMMDF__data, TFile } from "../../lib/fileSystem";
 
+type TReaderOption = "option1" | "option2";
+
 interface IProps {
   onChange?: (data: any) => void;
+  option?: TReaderOption;
 }
 
 function MMDFReader(props: IProps) {
-  const mmdfHandler = async (fileData: IMMDF__data, type: TFile) => {
+  const setItem = async (fileData: IMMDF__data, option?: TReaderOption) => {
+    const items = fileData.item ?? [];
+    switch (option) {
+      case "option1":
+        items.forEach(async (item) => {
+          const dbItem = await db.item.get({ name: item.name });
+          if (!dbItem) await db.item.add({ name: item.name, price: item.price });
+        });
+        break;
+      default:
+        items.forEach(async (item) => {
+          const dbItem = await db.item.get({ name: item.name });
+          if (dbItem) {
+            await db.item.update(item.id!, { price: item.price });
+          } else {
+            await db.item.add({ name: item.name, price: item.price });
+          }
+        });
+        break;
+    }
+  };
+
+  const setRecipe = async (fileData: IMMDF__data, option?: TReaderOption) => {
+    const recipes = fileData.recipe ?? [];
+    switch (option) {
+      case "option1":
+        recipes.forEach(async (recipe) => {
+          const dbRecipe = await db.recipe.get({ name: recipe.name });
+          delete recipe.id;
+          if (dbRecipe) {
+            await db.recipe.update(dbRecipe.id!, recipe);
+          } else {
+            await db.recipe.add(recipe);
+          }
+        });
+        break;
+      default:
+        recipes.forEach(async (recipe) => {
+          const dbRecipe = await db.recipe.get({ name: recipe.name });
+          delete recipe.id;
+          if (!dbRecipe) await db.recipe.add(recipe);
+        });
+        break;
+    }
+  };
+
+  const mmdfHandler = async (fileData: IMMDF__data, type: TFile, option?: TReaderOption) => {
     try {
       switch (type) {
         case "backup":
@@ -14,27 +63,10 @@ function MMDFReader(props: IProps) {
           await mmdfHandler(fileData, "recipe");
           break;
         case "item":
-          const items = fileData.item ?? [];
-          items.forEach(async (item) => {
-            const dbItem = await db.item.get(item.id!);
-            if (dbItem) {
-              await db.item.update(item.id!, { price: item.price });
-            } else {
-              await db.item.add({ name: item.name, price: item.price });
-            }
-          });
+          await setItem(fileData, option);
           break;
         case "recipe":
-          const recipes = fileData.recipe ?? [];
-          recipes.forEach(async (recipe) => {
-            const dbRecipe = await db.recipe.get({ name: recipe.name });
-            delete recipe.id;
-            if (dbRecipe) {
-              await db.recipe.update(dbRecipe.id!, recipe);
-            } else {
-              await db.recipe.add(recipe);
-            }
-          });
+          await setRecipe(fileData, option);
           break;
         case "itemLog":
           break;
@@ -57,7 +89,7 @@ function MMDFReader(props: IProps) {
       fileReader.onload = () => {
         try {
           const dataObj = JSON.parse(fileReader.result as string) as IMMDF;
-          mmdfHandler(dataObj.data, dataObj.meta.type);
+          mmdfHandler(dataObj.data, dataObj.meta.type, props.option);
           if (props.onChange) props.onChange(dataObj);
         } catch (err) {
           console.error(err);
