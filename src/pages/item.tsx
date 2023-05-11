f39329da5e1d66ad1c83a56c249232c34e307edf
pokycookie <0cookieboy0@gmail.com>
2023-05-11 21:15:49 +0900

    feat: item deletion feature

--- src/pages/item.tsx
+++ src/pages/item.tsx
@@ -5,9 +5,9 @@ import NumberInput from "../components/numberInput/numberInput";
 import Select, { SingleValue } from "react-select";
 import React, { useEffect, useState } from "react";
 import { IDBItem, db, IDBItemLog } from "../db";
-import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faPenToSquare, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { IReduxStore, RSetItemList } from "../redux";
+import { IReduxStore, RSetItemList, RSetModalID } from "../redux";
 import { Noti } from "../lib/notification";
 import Modal from "../components/modal/modal";
 import { checkDateEqual } from "../lib/time";
@@ -17,38 +17,12 @@ import EasyInput from "../components/easyInput/easyInput";
 function ItemPage() {
   const [item, setItem] = useState<number | null>(null);
   const [price, setPrice] = useState<number>(0);
-  const [addItem, setAddItem] = useState<string>("");
-  const [modal, setModal] = useState<boolean>(false);
 
   const itemList = useSelector<IReduxStore, IDBItem[]>((state) => {
     return state.itemList;
   }, shallowEqual);
 
   const dispatch = useDispatch();
-
-  const refreshDB = async () => {
-    const data = await db.item.toArray();
-    dispatch(RSetItemList(data));
-  };
-
-  const addHandler = () => {
-    // setModalOpen((prev) => (prev ? false : true));
-    if (addItem.trim() !== "") {
-      db.item
-        .add({ name: addItem, price: 0 })
-        .then(() => {
-          Noti.success("새로운 아이템이 추가되었습니다");
-        })
-        .catch((err) => {
-          console.error(err);
-          Noti.danger("새로운 아이템을 추가하지 못했습니다");
-        });
-    } else {
-      Noti.warning("아이템 이름을 작성해 주세요");
-    }
-    setAddItem("");
-    refreshDB();
-  };
 
   const selectHandler = (e: SingleValue<{ value: number; label: string }>) => {
     if (e) setItem(e.value);
@@ -59,8 +33,9 @@ function ItemPage() {
     setPrice(value);
   };
 
-  const textHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAddItem(e.target.value);
+  const refreshDB = async () => {
+    const data = await db.item.toArray();
+    dispatch(RSetItemList(data));
   };
 
   const editHandler = async () => {
@@ -128,25 +103,114 @@ function ItemPage() {
         </button>
       </div>
       <div className="btn-area">
+        <button className="add-btn circleBtn" onClick={() => dispatch(RSetModalID("itemEdit"))}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+        <button className="list-btn circleBtn" onClick={() => dispatch(RSetModalID("itemLog"))}>
+          <FontAwesomeIcon icon={faList} />
+        </button>
+      </div>
+      <Modal modalID="itemEdit" width="60%" height="30%" maxWidth="550px" autoClose>
+        <ItemEditModal />
+      </Modal>
+      <Modal modalID="itemLog" width="60%" height="70%" maxWidth="550px" autoClose>
+        <ItemLogModal />
+      </Modal>
+    </div>
+  );
+}
+
+function ItemEditModal() {
+  const [addItem, setAddItem] = useState<string>("");
+
+  const dispatch = useDispatch();
+
+  const itemList = useSelector<IReduxStore, IDBItem[]>((state) => {
+    return state.itemList;
+  }, shallowEqual);
+
+  const refreshDB = async () => {
+    const data = await db.item.toArray();
+    dispatch(RSetItemList(data));
+  };
+
+  const addHandler = () => {
+    if (addItem.trim() !== "") {
+      db.item
+        .add({ name: addItem, price: 0 })
+        .then(() => {
+          Noti.success("새로운 아이템이 추가되었습니다");
+        })
+        .catch((err) => {
+          console.error(err);
+          Noti.danger("새로운 아이템을 추가하지 못했습니다");
+        });
+    } else {
+      Noti.warning("아이템 이름을 작성해 주세요");
+    }
+    setAddItem("");
+    refreshDB();
+  };
+
+  const deleteHandler = async (item: IDBItem) => {
+    const recipe = await db.recipe
+      .filter((recipe) => recipe.items.some((recipeItem) => recipeItem.id === item.id!))
+      .toArray();
+    if (recipe.length === 1) {
+      Noti.warning(`해당 아이템은 '${recipe[0].name}'에서 사용중이므로 삭제할 수 없습니다.`);
+    } else if (recipe.length > 0) {
+      Noti.warning(
+        `해당 아이템은 '${recipe[0].name}' 외 ${
+          recipe.length - 1
+        }개의 레시피에서 사용중이므로 삭제할 수 없습니다.`
+      );
+    } else {
+      db.item
+        .delete(item.id!)
+        .then(() => {
+          Noti.success(`'${item.name}'이 삭제되었습니다.`);
+          refreshDB();
+        })
+        .catch((err) => {
+          console.error(err);
+          Noti.danger(`'${item.name}'을 삭제하지 못했습니다.`);
+        });
+    }
+  };
+
+  const textHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddItem(e.target.value);
+  };
+
+  return (
+    <div className="item__edit--container">
+      <div className="add--area">
         <input
           type="text"
           placeholder="추가할 아이템을 입력하세요"
+          autoFocus
           value={addItem}
           onChange={textHandler}
           onKeyDown={(e) => {
             if (e.nativeEvent.key === "Enter") addHandler();
           }}
         />
-        <button className="add-btn circleBtn" onClick={addHandler}>
-          <FontAwesomeIcon icon={faPlus} />
-        </button>
-        <button className="list-btn circleBtn" onClick={() => setModal(true)}>
-          <FontAwesomeIcon icon={faList} />
-        </button>
+        <button onClick={addHandler}>추가</button>
       </div>
-      <Modal open={modal} onClick={() => setModal(false)} width="60%" height="70%" maxWidth="550px">
-        <ItemLogModal />
-      </Modal>
+      <ul>
+        {itemList.map((e) => {
+          return (
+            <li key={e.id!}>
+              <p>{e.name}</p>
+              <FontAwesomeIcon
+                className="icon"
+                icon={faTrashAlt}
+                onClick={() => deleteHandler(e)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
